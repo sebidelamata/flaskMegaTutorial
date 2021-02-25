f45b44e1bb1302fae7caae1cda83dad20f974017
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-25 17:15:27 -0500

    added a user variable with some psuedo posts in app/routes.py, added a for loop with Jinja in the app/templates/index.html to display the posts

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    # user is the account holder (you if you are using the page)
     user = {'username':'Sebi'}
-    return render_template('index.html', user=user)
+    # posts is a list containing nested dictionaries (pretty much json)
+    # that holds posts of users connected to the account of the current user (you)
+    posts = [
+        {
+            'author': {'username': 'Jerry'},
+            'body': 'My hair smells like old turkey and this upsets me.'
+        },
+        {
+            'author': {'username': 'Joe'},
+            'body': 'Bet no cap hunnid'
+        }
+    ]
+    return render_template('index.html', user=user, post=posts)
