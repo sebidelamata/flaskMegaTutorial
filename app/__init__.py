8fa3197f8d44936ab9279df0a94c0ed65d1b8e85
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-26 11:26:14 -0500

    added config class script and referenced it in __init__.py to generate secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 # import packages
 from flask import Flask
+from config import Config
 
 # create application object consisting of a Flask instance
 app = Flask(__name__)
+# after we create the app object we tell the app to generate
+# the secret key from the Config class in the config file
+# to prevent CSRF attacks from our forms
+app.config.from_object(Config)
 
 # the app object imports a routes object, which doesnt exist yet.
 from app import routes
