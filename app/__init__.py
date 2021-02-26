b83c52bbe8f3a64f4e627f26cd1b510d9210f843
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-26 15:42:02 -0500

    created database and migration engine objects in the __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
 # import packages
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # create application object consisting of a Flask instance
 app = Flask(__name__)
+
 # after we create the app object we tell the app to generate
 # the secret key from the Config class in the config file
 # to prevent CSRF attacks from our forms
 app.config.from_object(Config)
 
-# the app object imports a routes object, which doesnt exist yet.
-from app import routes
+# this db object represents the database
+db = SQLAlchemy(app)
+
+# this migrate object represents the data migration engine
+migrate = Migrate(app, db)
+
+# the app object imports a routes object, models defines the structure of the sqlite database.
+from app import routes, models
