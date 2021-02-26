98460d5bc4508efca21338e2ec460691c1963ed2
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-26 15:10:50 -0500

    created POST method in login view and logic for form submission and view redirection

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -23,9 +23,17 @@ def index():
     return render_template('index.html', user=user, posts=posts)
 
 # This view function is for the login page
-@app.route('/login')
+# The wrapper function invokes both the get in post methods to
+# receive the form and output to the desired landing page
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     # create a variable form that is an instance of the login form class
     form = LoginForm()
+    # if the for is validated the user is sent to their home page,
+    # otherwise stay on the login page
+    if form.validate_on_submit():
+        # the flash function with flash the dialogue to the user
+        flash('Login requested for user {}, remember_me{|'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     # return the login page template to the user
     return render_template('login.html', title='Log in', form=form)
