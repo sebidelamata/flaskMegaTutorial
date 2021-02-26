6d966b36758e27caee38d6cffad629a130230349
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-26 17:00:27 -0500

    setting up sqlite db

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # import our database object
 from app import db
+from datetime import datetime
 
+# users table
 class User(db.Model):
     # id column is an int primary key
     id = db.Column(db.Integer, primary_key=True)
@@ -11,8 +13,26 @@ class User(db.Model):
     # password hash is the encrypted password that is stored in the database
     # (not the actual password)
     password_hash = db.Column(db.String(128))
+    # defines a relationship with post tab
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # this method tells python how to print the objects in this
     # class instead of just returning the object
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# posts table
+class Post(db.Model):
+    # post id is primary key int
+    id = db.Column(db.Integer, primary_key=True)
+    # body is the actual post that can be up to 500 characters
+    body = db.Column(db.String(500))
+    # timestamp is the time of the posting in UTC
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # user id is the userid and is linked to the users table
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # this method tells python how to print the objects in this
+    # class instead of just returning the object
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
-from app import app
+# import modules
+from app import app, db
+from app.models import User, Post
+
+# this adds the database and models insto the shell session
+# the wrapper specifies that this function is a shell context function
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
