7e840f21cc247613a52b84ce4cc092f0c1287d5c
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-02-26 15:36:55 -0500

    defined the path and name for the sqlite directory

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 # Secret key configuration
 import os
 
+# define our base directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
+    # this variable is the user's secret key or the hardcoded backup string
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # this variable establishes the path and name for the database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # we do not need to signal the app everytime a change is made to the database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
