a1d7855c90ac7404a453be2cf5d595b62d323332
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-03-01 20:18:27 -0500

    added a rotating logfile handler

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 # create application object consisting of a Flask instance
 app = Flask(__name__)
@@ -27,5 +30,51 @@ login = LoginManager(app)
 # of our view functions handles the login
 login.login_view = 'login'
 
-# the app object imports a routes object, models defines the structure of the sqlite database.
+# the app object imports a routes object,
+# models defines the structure of the sqlite database.
+# errors is for our custom error handling
 from app import routes, models, errors
+
+# this only runs when the app is not in debug mode
+if not app.debug:
+    # if the config file has a value for mail-server then auth is set to none
+    if app.config['MAIL_SERVER']:
+        auth = None
+        # if there is a value for the mail username and password lets use those to authenticate
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        # secure is set to none
+        secure = None
+        # if the file has a value for MAIL use tls then secure is set to that value
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        # an instance of the smtp service is create with the variables plugged in
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog failure',
+            credentials=auth, secure=secure
+        )
+        # the level is set to a logging error
+        mail_handler.setLevel(logging.ERROR)
+
+    # if there isn't already a subdirectory named logs, make one
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    # file handler is an instance of a RotatingFileHandler class
+    # rotating logs makes sure we don't have a giant file when the
+    # app have been running for awhile. We limit the size of the
+    # logfile and only keep the last ten logfiles
+    file_handler = RotatingFileHandler('logs/microblog.log',
+                                       maxBytes=10240,
+                                       backupCount=10)
+    # a little formatting for our log messages
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
--- config.py
+++ config.py
@@ -12,3 +12,11 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     # we do not need to signal the app everytime a change is made to the database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # find mail server variables if they are set in the environment
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # email is on default 25 unless otherwise stated
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 25
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # these people will receive error logs
+    ADMINS = ['sebidelamata@gmail.com']
