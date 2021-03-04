17a74abf0c0e66aaf9a97324432c0d19a567f4de
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-03-04 17:05:11 -0500

    finished email stuff

--- app/email.py
+++ app/email.py
@@ -1,15 +1,20 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread
 
-# here we assign fields for our message and send it
+def send_async_email(app, msg):
+    with app.context():
+        mail.send(msg)
+
+# here we assign fields for our message and send it through asynchronous enail
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject,
                   sender=sender,
                   recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 # here we create a password reset token and send an email
 # using the function we created above
